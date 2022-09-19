633aeb4bcb291e8efd49ee8f6bbec788fde0f944
Mansour <84951627+marzban-dev@users.noreply.github.com>
2022-09-19 15:05:19 +0430

    Update project

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,6 +1,6 @@
 import 'styles/globals.css'
 import type {AppProps} from 'next/app'
-import {dehydrate,QueryClient, QueryClientProvider, Hydrate} from "@tanstack/react-query";
+import {dehydrate, QueryClient, QueryClientProvider, Hydrate} from "@tanstack/react-query";
 import {ParallaxProvider} from 'react-scroll-parallax';
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 import {SessionProvider} from "next-auth/react";
@@ -17,9 +17,7 @@ function MyApp({Component, pageProps}: AppProps) {
                 <title>Tv Show</title>
             </Head>
             <QueryClientProvider client={queryClient.current}>
-                {/* @ts-ignore */}
                 <Hydrate state={pageProps.dehydratedState}>
-                    {/* @ts-ignore */}
                     <SessionProvider session={pageProps.session}>
                         <ParallaxProvider>
                             <Component {...pageProps} />
--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -6,13 +6,6 @@ export default function Document() {
         <Html className="font-lato">
             <Head>
                 <title>Gallery</title>
-                <Script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"/>
-                <Script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/vendors/jquery.easings.min.js"
-                        integrity="sha512-rXZZDfRSa6rsBuT78nRTbh1ccwpXhTCspKUDqox3hUQNYdjB6KB6mSj6mXcB9/5F5wODAJnkztXPxzkTalp11w==" crossOrigin="anonymous" referrerPolicy="no-referrer"/>
-                <Script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/vendors/scrolloverflow.min.js"
-                        integrity="sha512-FzESM/E7XJBqcJyrXa08gRcpp5rDHO661C0L3vH4NsZfUWUsjN4+t6Lg8h+e8TMR2aYijIrcT+CPGq7tSugRzA==" crossOrigin="anonymous" referrerPolicy="no-referrer"/>
-                <Script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/jquery.fullpage.min.js" integrity="sha512-bxzECOBohzcTcWocMAlNDE2kYs0QgwGs4eD8TlAN2vfovq13kfDfp95sJSZrNpt0VMkpP93ZxLC/+WN/7Trw2g=="
-                        crossOrigin="anonymous" referrerPolicy="no-referrer"/>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/jquery.fullpage.min.css"
                       integrity="sha512-q54FvbV+gGBn+NvgaD4gpJ7w4wrO00DgW7Rx503PIhrf0CuMwLOwbS+bXgOBFSob+6GVy1HDPfaRLJ8w2jiS4g==" crossOrigin="anonymous" referrerPolicy="no-referrer"/>
             </Head>
@@ -21,6 +14,14 @@ export default function Document() {
             <Main/>
             <NextScript/>
             </body>
+            <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js" />
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/vendors/jquery.easings.min.js"
+                    integrity="sha512-rXZZDfRSa6rsBuT78nRTbh1ccwpXhTCspKUDqox3hUQNYdjB6KB6mSj6mXcB9/5F5wODAJnkztXPxzkTalp11w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/vendors/scrolloverflow.min.js"
+                    integrity="sha512-FzESM/E7XJBqcJyrXa08gRcpp5rDHO661C0L3vH4NsZfUWUsjN4+t6Lg8h+e8TMR2aYijIrcT+CPGq7tSugRzA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/jquery.fullpage.min.js"
+                    integrity="sha512-bxzECOBohzcTcWocMAlNDE2kYs0QgwGs4eD8TlAN2vfovq13kfDfp95sJSZrNpt0VMkpP93ZxLC/+WN/7Trw2g=="
+                    crossOrigin="anonymous" referrerPolicy="no-referrer"/>
         </Html>
     );
 }
