079cb3c98595223c072d5693e81197161a3d9219
Mansour <84951627+marzban-dev@users.noreply.github.com>
2022-09-19 15:27:56 +0430

    Update project ...

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -6,18 +6,17 @@ export default function Document() {
         <Html className="font-lato">
             <Head>
                 <title>Gallery</title>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/jquery.fullpage.min.css"
-                      integrity="sha512-q54FvbV+gGBn+NvgaD4gpJ7w4wrO00DgW7Rx503PIhrf0CuMwLOwbS+bXgOBFSob+6GVy1HDPfaRLJ8w2jiS4g==" crossOrigin="anonymous" referrerPolicy="no-referrer"/>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/jquery.fullpage.min.css" crossOrigin="anonymous" referrerPolicy="no-referrer"/>
             </Head>
             <body>
             <div id="portal-container"></div>
             <Main/>
             <NextScript/>
             </body>
-            <Script strategy="beforeInteractive" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js" />
-            <Script strategy="beforeInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/vendors/jquery.easings.min.js" />
-            <Script strategy="beforeInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/vendors/scrolloverflow.min.js" />
-            <Script strategy="beforeInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/jquery.fullpage.min.js" />
+            <Script strategy="beforeInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" crossOrigin="anonymous"/>
+            <Script strategy="beforeInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/vendors/jquery.easings.min.js" crossOrigin="anonymous"/>
+            <Script strategy="beforeInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/vendors/scrolloverflow.min.js" crossOrigin="anonymous"/>
+            <Script strategy="beforeInteractive" src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/jquery.fullpage.min.js" crossOrigin="anonymous"/>
         </Html>
     );
 }
