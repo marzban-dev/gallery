8692bfb128072475ec804d9746382f5f2ff08590
Mansour <84951627+marzban-dev@users.noreply.github.com>
2022-09-19 14:29:50 +0430

    Update _app.page.tsx

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,11 +1,12 @@
 import 'styles/globals.css'
 import type {AppProps} from 'next/app'
-import {QueryClient, QueryClientProvider, Hydrate} from "@tanstack/react-query";
+import {dehydrate,QueryClient, QueryClientProvider, Hydrate} from "@tanstack/react-query";
 import {ParallaxProvider} from 'react-scroll-parallax';
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 import {SessionProvider} from "next-auth/react";
 import {Fragment, useRef} from "react";
 import Head from "next/head";
+import {GetStaticProps} from "next";
 
 function MyApp({Component, pageProps}: AppProps) {
     const queryClient = useRef(new QueryClient());
@@ -16,7 +17,9 @@ function MyApp({Component, pageProps}: AppProps) {
                 <title>Tv Show</title>
             </Head>
             <QueryClientProvider client={queryClient.current}>
+                {/* @ts-ignore */}
                 <Hydrate state={pageProps.dehydratedState}>
+                    {/* @ts-ignore */}
                     <SessionProvider session={pageProps.session}>
                         <ParallaxProvider>
                             <Component {...pageProps} />
@@ -28,5 +31,18 @@ function MyApp({Component, pageProps}: AppProps) {
         </Fragment>
     )
 }
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+    const queryClient = new QueryClient();
+
+    // await queryClient.prefetchQuery();
+
+    return {
+        props: {
+            dehydratedState: dehydrate(queryClient),
+        },
+    };
+};
+
 
 export default MyApp
